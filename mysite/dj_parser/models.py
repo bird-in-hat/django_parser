218b4b3b2b5db314ef2dc93a6a510745a9a9cc9c
bird-in-hat <buldakov.95@mail.ru>
2018-06-28 00:01:52 +0300

    Model create

--- mysite/dj_parser/models.py
+++ mysite/dj_parser/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Page(models.Model):
+
+    page_url = models.CharField(max_length=400,  default='')
+
+    h1_count = models.IntegerField(default=0)
+    h2_count = models.IntegerField(default=0)
+    h3_count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.page_url
+
+
+class Link(models.Model):
+    page = models.ForeignKey(Page, related_name='links', on_delete=models.CASCADE)
+    a_url = models.CharField(max_length=400,  default='')
+
+    def __str__(self):
+        return self.a_url
+
